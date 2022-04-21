014e3c6202e78f51d8257f3c3dca436c94a55582
Guilhem <guilhem.cros.38@gmail.com>
2022-04-21 18:42:09 +0200

    creation d'une interface du gestionaire de tolerence

---
+++ graphic/tolerenceInterface/TolerenceManagingFrame.java
@@ -0,0 +1,22 @@
+package pixelFinder.graphic.tolerenceInterface;
+
+import pixelFinder.modifier.TolerenceManager;
+
+import javax.swing.*;
+
+public class TolerenceManagingFrame extends JFrame {
+
+    JScrollPane view = new JScrollPane();
+
+    TolerenceManager tolerenceManager;
+
+    void addTolerencePane(){
+
+
+
+    }
+
+
+
+
+}
